348cc55fb2bc5301eaeebceda629b80b2c277064
Andres R. Villarroel <andresrvillarroel@gmail.com>
2022-09-24 21:45:41 -0300

    Modified pipeline (#4)
    * Modified pipeline
    
    * Fixed pipeline
    
    * Added docker-compose, modified listening port to 0.0.0.0
    
    Co-authored-by: Andres Raul Villarroel <andresrv94@Andress-MacBook-Pro.local>

--- main.go
+++ main.go
@@ -25,5 +25,5 @@ func main() {
 	router.GET("/albums/:id", getAlbumByID)
 	router.POST("/albums", postAlbums)
 	router.GET("/btcprice", getBtcPrice)
-	router.Run("localhost:8080")
+	router.Run("0.0.0.0:8080")
 }
